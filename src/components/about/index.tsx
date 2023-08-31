3df6c9542d3b7f5d30efb4d27e220732535b22a4
Bryan Fernandes <bryanfernandes8@gmail.com>
2023-08-31 11:58:34 -0300

    feat: base layout and resume

---
+++ src/components/about/index.tsx
@@ -0,0 +1,20 @@
+import "./style.css"
+
+export default function About() {
+    return <div className="about">
+        <p>Sou um entusiasta de tecnologia e desenvolvedor Fullstack apaixonado por criar soluções inovadoras e enfrentar desafios complexos. Atualmente, estou perseguindo meu diploma em Ciências da Computação, buscando aprofundar meu conhecimento técnico e aplicá-lo em projetos significativos.</p>
+        <p>Estou constantemente em busca de oportunidades empolgantes que me permitam aplicar minhas habilidades, paixões e conhecimento enquanto cresço como desenvolvedor e profissional de tecnologia. Seja através de desafios técnicos complexos, colaboração em equipe ou aprendizado contínuo, estou pronto para enfrentar novas jornadas e alcançar resultados excepcionais.</p>
+        
+        {/* <p>Tenho um compromisso inabalável com a qualidade em tudo o que faço. Escrevo código limpo e bem documentado, visando não apenas a funcionalidade, mas também a legibilidade e a manutenibilidade a longo prazo. Minha dedicação à excelência se reflete em cada aspecto do meu trabalho.</p> */}
+        {/* <p>Com um ano de experiência sólida como estagiário da <a href="https://www.frameworkdigital.com.br/">FrameWork Digital</a>, trabalhando em uma variedade de projetos desafiadores. Minha paixão pela tecnologia e minha capacidade de aprender rapidamente me permitiram adquirir conhecimentos abrangentes em desenvolvimento web e contribuir de maneira significativa para equipes de desenvolvimento colaborativas.</p> */}
+        <h3>Alguns pontos chave sobre mim</h3>
+        <ul>
+            <li>Experiência Prática e Aprendizado Ágil</li>
+            <li>Desenvolvimento Colaborativo</li>
+            <li>Sede por Desafios e Crescimento</li>
+            <li>Compromisso com a Qualidade e Aprendizado Contínuo</li>
+            <li>Foco em Qualidade e Excelência</li>
+        </ul>
+        <p>Quando não estou no computador, costumo me exercitar, jogar fortnite ou sair com os amigos.</p>
+    </div>
+}
