b60dde999db4733c82ca1d2bfd9e33cc7155bb82
Bryan Fernandes <bryanfernandes8@gmail.com>
2023-08-30 23:48:41 -0300

    initial config

---
+++ src/components/projects/projects.ts
@@ -0,0 +1,48 @@
+import whatslab from '../../assets/images/whatslab.webp'
+import labefy from '../../assets/images/labefy.png'
+import astromatch from '../../assets/images/astromatch.png'
+import githubsearch from '../../assets/images/githubsearch.png'
+import labex from '../../assets/images/labexjpg.jpg'
+
+export const projects = [
+    {
+        name: 'Whatslab',
+        img: whatslab,
+        link: 'https://sloppy-map.surge.sh/',
+        repo: 'https://github.com/bammido/Whatslab',
+        description: 'Envie mensagens instantâneas para qualquer um a qualquer hora.',
+
+    },
+    {
+        name: 'Labex',
+        img: labex,
+        link: 'https://helpless-spot.surge.sh/',
+        repo: 'https://github.com/bammido/Labex',
+        description: 'App de reservas de viagens, porém espaciais!',
+
+    },
+    {
+        name: 'AstroMatch',
+        img: astromatch,
+        link: 'https://selfish-sink.surge.sh/',
+        repo: 'https://github.com/bammido/AstroMatch',
+        description: 'Buscando sua cara metade? a tampa da sua panela? Esse app é sua solução.',
+
+    },
+    {
+        name: 'Labefy',
+        img: labefy,
+        link: 'https://nine-pin.surge.sh/',
+        repo: 'https://github.com/bammido/Labefy',
+        description: 'Precisa organizar suas músicas? Esse app é perfeito para isso.',
+
+    },
+    {
+        name: 'GithubSearch',
+        img: githubsearch,
+        link: 'https://lying-meal.surge.sh/',
+        repo: 'https://github.com/bammido/GithubSearch',
+        description: 'Busque por repositórios no github bem aqui!',
+
+    },
+]
